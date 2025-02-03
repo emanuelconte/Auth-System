27977fbb8021d9717dfbfa329abf279131851629
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-03 18:46:40 -0300

    adding authController

--- src/main/java/auth/sys/controller/AuthController.java
+++ src/main/java/auth/sys/controller/AuthController.java
@@ -1,4 +1,21 @@
 package auth.sys.controller;
 
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
+        userService.registerUser(registerRequest);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        String token = userService.loginUser(loginRequest);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
 }
