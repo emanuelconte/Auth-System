90b627a5b8fa5b08a406e441bb8457c803e78496
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-03 18:51:57 -0300

    adding JwtUtil

--- src/main/java/auth/sys/security/JwtUtil.java
+++ src/main/java/auth/sys/security/JwtUtil.java
@@ -1,4 +1,24 @@
 package auth.sys.security;
 
+@Component
 public class JwtUtil {
+
+    private String SECRET_KEY = "seuSegredoSuperSecreto";
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration();
+    }
 }
