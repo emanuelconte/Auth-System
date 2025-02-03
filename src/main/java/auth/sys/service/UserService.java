5d0a72c6fa2c5a099bcfab60eb1f5b25667c712d
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-03 18:48:41 -0300

    adding User service

--- src/main/java/auth/sys/service/UserService.java
+++ src/main/java/auth/sys/service/UserService.java
@@ -1,4 +1,40 @@
 package auth.sys.service;
 
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public void registerUser(RegisterRequest registerRequest) {
+        if (userRepository.findByUsername(registerRequest.getUsername()).isPresent()) {
+            throw new UserAlreadyExistsException("Username already exists!");
+        }
+
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setRole("USER");
+        user.setEnabled(true);
+
+        userRepository.save(user);
+    }
+
+    public String loginUser(LoginRequest loginRequest) {
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials!");
+        }
+
+        return jwtUtil.generateToken(user.getUsername());
+    }
 }
