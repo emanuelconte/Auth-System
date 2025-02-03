7bd61235b361ffb3c1e2c973faa2ddfa57e2d363
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-03 18:49:08 -0300

    adding JwtService

--- src/main/java/auth/sys/service/JwtService.java
+++ src/main/java/auth/sys/service/JwtService.java
@@ -1,4 +1,29 @@
 package auth.sys.service;
 
+@Service
 public class JwtService {
+
+    private String SECRET_KEY = "seuSegredoSuperSecreto";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 24 horas
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration();
+    }
 }
