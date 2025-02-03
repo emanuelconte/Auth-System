49044039cb939f86f4b78d7fbfaf702bd7cba58d
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-03 18:50:29 -0300

    adding LoginRequest

--- src/main/java/auth/sys/dto/LoginRequest.java
+++ src/main/java/auth/sys/dto/LoginRequest.java
@@ -1,4 +1,24 @@
 package auth.sys.dto;
 
 public class LoginRequest {
+    private String username;
+    private String password;
+
+    // Getters e Setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
