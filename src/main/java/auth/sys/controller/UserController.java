c1207dc5f3e7a5c7b1b94a5e6d2719aa4d2fa492
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-04 16:22:25 -0300

    adding UserUpdateRequest

--- src/main/java/auth/sys/controller/UserController.java
+++ src/main/java/auth/sys/controller/UserController.java
@@ -1,5 +1,6 @@
 package auth.sys.controller;
 
+import auth.sys.dto.UserUpdateRequest;
 import auth.sys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
